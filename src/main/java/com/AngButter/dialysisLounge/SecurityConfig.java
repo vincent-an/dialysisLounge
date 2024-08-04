4ef64cf765e35accd3f64d51e830b4ed203f1088
vincent-an <xxv211@naver.com>
2024-08-05 02:09:51 +0900

    feat : first commit - 7/20일 새벽기준 html,css 적용 - 홈 화면 및 생활관리법 링크 적용 완료
    feat : second commit
    - 7/21일 전체적인 html 코드 버튼으로 수정 및 css 코드 수정 완료
    - 생활관리법 -> 운동, 응급가이드 창 링크 연결 완료
    - 현재 본인 창 Pretendard 폰트 적용 안됨(해결)
    
    feat : third comit
    - 7/26일 프로젝트 최신까지 업데이트 완료
    - 각 링크 연결 완료
    - 뒤로가기, 홈 이미지 같이 있을때 언더바 생기는 오류 발생
    - 폰트 Pretendard 적용 완료
    - map.html 성남시 투석병원 위치 12곳 추가 등록 완료
    
    feat : 24.07.31
    -h2데이터 베이스 연동완료
    -build.gradle 추가
    -test페이지로 위치 추가해야됨
    -커스텀 오버레이 추가완료
    -이에 맞는 병원 사진 추가
    -아직 여러개 되는지는 미지수
    
    feat 24.08.01
    -map.html 최신본까지 수정완료
    -diet.html, recipe.html 추가 완료
    -controller 및 애너테이션 수정 완료
    
    feat 24.08.04
    -게시판 생성
    -비디오 마저 연결
    
    feat 24.08.05 02:09분 최신본 업로드

--- src/main/java/com/AngButter/dialysisLounge/SecurityConfig.java
+++ src/main/java/com/AngButter/dialysisLounge/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -15,7 +18,17 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
         ;
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
